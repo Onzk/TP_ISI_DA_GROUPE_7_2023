20ca82dddaecfb0e8d892f829230bb55c54564ba
KOUDOSSOU <koudossoujustin@gmail.com>
2023-04-02 16:02:26 +0200

    Initialisation du projet

---
+++ src/main/java/ucao/isi/G6/Main.java
@@ -0,0 +1,13 @@
+package ucao.isi.G6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
